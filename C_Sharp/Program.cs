97299fbcf16bbc894a443ea09c2821c08c952fbc
Darpannn <darpanapatel2003@gmail.com>
2026-08-30 16:09:34 +0530

    Initial commit

---
+++ C_Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿// using System;
+
+// 	public class Darpan
+// 	{
+// 		public static void Main(string[] args)
+// 		{
+// 			int age = 22;
+// 			string name = "Darpan";
+// 			string collage = "GecGN";
+// 			string branch = "CE";
+// 			double CGPA = 7.6;
+
+// 			Console.WriteLine($"My name is {name}.");
+// 			Console.WriteLine($"I am {age} year old.");
+// 			Console.WriteLine($"I study at {collage}.");
+// 			Console.WriteLine($"My branch is {branch}.");
+// 			Console.WriteLine($"i got {CGPA} CGPA in last semester.");
+// 		}
+// 	}
---
+++ C_Sharp/first.cs
@@ -0,0 +1,26 @@
+using System;
+public class First
+{
+        public static void Main(string[] args)
+    {
+        Console.Write("Enter Your Marks : ");
+        int mark = int.Parse(Console.ReadLine());
+
+        if (mark >= 90)
+        {
+            Console.WriteLine("A - Grade");
+        }
+        else if (mark >= 80)
+        {
+            Console.WriteLine("B - Grade");
+        }
+        else if (mark >= 70)
+        {
+            Console.WriteLine("C - Grade");
+        }
+        else
+        {
+            Console.WriteLine("You Failed in Exam");
+        }
+    }
+}

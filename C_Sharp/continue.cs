c7dfd8521d338c6200f257f2865ff85428010d8c
Darpannn <darpanapatel2003@gmail.com>
2026-09-01 22:14:42 +0530

    Practice C# - Day-3

--- C_Sharp/continue.cs
+++ C_Sharp/continue.cs
@@ -1,19 +1,19 @@
-using System;
+// using System;
 
-public class Name
-{
-    public static void Main(string[] args)
-    {
-        for(int i = 1; i >= 10; i++)
-        {
-            if(i % 2 != 0)
-            {
-                Console.Write(i + " ");
-            }
-            else
-            {
-                continue;
-            }
-        }
-    }
-}
+// public class Name
+// {
+//     public static void Main(string[] args)
+//     {
+//         for(int i = 1; i >= 10; i++)
+//         {
+//             if(i % 2 != 0)
+//             {
+//                 Console.Write(i + " ");
+//             }
+//             else
+//             {
+//                 continue;
+//             }
+//         }
+//     }
+// }
---
+++ C_Sharp/marks.cs
@@ -0,0 +1,31 @@
+// using System;
+
+// public class First
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.Write("Enter Your Marks: ");
+//         int mark = int.Parse(Console.ReadLine());
+
+//         if (mark >= 90)
+//         {
+//             Console.WriteLine("A - Grade");
+//         }
+//         else if (mark >= 80)
+//         {
+//             Console.WriteLine("B - Grade");
+//         }
+//         else if (mark >= 70)
+//         {
+//             Console.WriteLine("C - Grade");
+//         }
+//         else if (mark >= 60)
+//         {
+//             Console.WriteLine("D - Grade");
+//         }
+//         else
+//         {
+//             Console.WriteLine("You Failed in Exam");
+//         }
+//     }
+// }

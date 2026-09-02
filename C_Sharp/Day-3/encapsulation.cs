7d519dc939e8a6c77dcdc4681147c688e3d43fcb
Darpannn <darpanapatel2003@gmail.com>
2026-09-02 23:24:02 +0530

    Practice new topic in C#

---
+++ C_Sharp/Day-3/encapsulation.cs
@@ -0,0 +1,33 @@
+// using System;
+
+// public class Bank
+// {
+//     private double balance;
+
+//     public void deposite(double amount)
+//     {
+//         if (amount > 0)
+//         {
+//             balance += amount;
+//         }
+//     }
+
+//     public void Getbalance()
+//     {
+//         Console.WriteLine($"Your Balance is : {balance}");
+//     }
+// }
+
+// public class Encap
+// {
+//     public static void Main(string[] args)
+//     {
+//         Bank darpan = new Bank();
+
+//         darpan.deposite(1000);
+//         darpan.deposite(500);
+
+//         darpan.Getbalance();
+
+//     }
+// }
---
+++ C_Sharp/Day-3/inherit.cs
@@ -0,0 +1,35 @@
+// using System;
+
+// public class Person
+// {
+//     public string name{get; set;}= " ";
+
+//     public void Intro()
+//     {
+//         Console.WriteLine($"Hello, I am {name}");
+//     }
+// }
+
+// public class Student : Person
+// {
+//     public string collage{get; set;} = " ";
+
+//     public void Study()
+//     {
+//         Console.WriteLine($"{name} is study at {collage}");
+//     }
+// }
+
+// public class Inherit
+// {
+//     public static void Main(string[] args)
+//     {
+//         Student darpan = new Student();
+
+//         darpan.name="Darpan Patel";
+//         darpan.collage="GecGN";
+
+//         darpan.Intro();
+//         darpan.Study();
+//     }
+// }

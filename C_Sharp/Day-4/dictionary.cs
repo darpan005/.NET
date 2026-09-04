153d61e5eca5057a98af902ece8597a2d2e99a20
Darpannn <darpanapatel2003@gmail.com>
2026-09-04 23:44:36 +0530

    c# practice Day-5

---
+++ C_Sharp/Day-4/dictionary.cs
@@ -0,0 +1,19 @@
+// using System;
+// using System.Collections.Generic;
+
+// public class Dictionary
+// {
+//     public static void Main(string[] args)
+//     {
+//         Dictionary<string, int> students = new Dictionary<string, int>();
+
+//         students.Add("darpan",22);
+//         students.Add("parth",19);
+//         students.Add("vishal",18);
+
+//         Console.WriteLine(students["darpan"]);
+//         Console.WriteLine(students["vishal"]);
+//         Console.WriteLine(students["parth"]);
+
+//       }
+// }
--- C_Sharp/obj/Debug/net10.0/C_Sharp.AssemblyInfo.cs
+++ C_Sharp/obj/Debug/net10.0/C_Sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C_Sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a93efaff49761b6c670bd45c56d4cf5d13a37ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccabe77b72485836eef7c1770ea060d5b1e35d27")]
 [assembly: System.Reflection.AssemblyProductAttribute("C_Sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C_Sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
